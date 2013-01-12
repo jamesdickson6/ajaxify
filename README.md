1ed59599d2dd64c219a70600252a17566c92c673
James Dickson <jdickson@bertramcapital.com>
2013-01-11 21:14:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+## Ajaxify
+
+This does something similar to [PJAX](See https://github.com/defunkt/jquery-pjax)
+Consider using PJAX instead, which is more robust and has caching and other goodness.
+
+This library makes the browser back/next navgiation work with an application that uses AJAX to load page content.
+As well as make the browser address bar a usable representation of the current page.
+For that reason,  Link URLs (href) should be valid without **format=ajax**.  
+This makes the site nicely and easily degenerate to plain old HTML.
+
+This leverages browser support for html5's pushState.
+
+BROWSER SUPPORT
+Feature           │ Chrome  │ Firefox (Gecko)  │  IE  │ Opera  │ Safari
+──────────────────┼─────────┼──────────────────┼──────┼────────┼───────
+replace/pushState │   5     │    4.0 (2.0)     │  10  │ 11.50  │  5.0
+history.state     │  18     │    4.0 (2.0)     │  10  │ 11.50  │  ---
+
+Without browser support, the only consequence should be that the Back button won't reload content via ajax.
+
+
+
+## Dependencies
+* [jQuery &#8805; 1.7.2](http://jquery.com/)
+
+## Usage
+Include the library
+```html
+<od:javascript src="jquery/jquery.ajaxify-1.0.min"/>
+
+Ajaxify all the links with class="ajaxLink"
+```js
+$(document).ajaxify(".ajaxLink", {update:"#primary-content"})
+```
+
+Render your links with html attributes
+```html
+<a class="ajaxLink" href="/mypage" update="#mycontainer" jsbefore="someFunction();" format="ajax">My Page</a>
+```
+
+I've also written a grails TagLib to just this..
+
+## Link Handling
+The default behavior of **Ajaxify.defaultHandler** might be all you ever need.  
+It supports lots of parameters.  Check out the code to see what options (html attributes) might be useful to you.
+
+You can write your own javascript handler function and then use that instead. Like this:
+```js
+function myCoolHandler(state) { ... }
+$(document).ajaxify(".ajaxCool", {handle: myCoolHandler})
+```
+Or customize a specific link:
+```html
+<a class="ajaxLink" handle="myCoolHandler">My Page</a>
+```
+
+NOTE: You don't have to use anchor tags, you can ajaxify any type of element.
+
+## Rendering
+Your web application will need to support **format=ajax** and render responses without their page layout.  
+You can, of course, customize the format parameter value
+
+
---
+++ jquery.ajaxify.js
@@ -0,0 +1,212 @@
+//  jquery.ajaxify.js
+//  https://github.com/sixjameses/ajaxify
+//  author: jdickson@bertramcapital.com
+
+var Ajaxify = {
+  version : '1.0',
+
+  defaultRequestFormat : 'ajax',
+  isInitialized : false,
+  isHandling : false, // handling link actions?
+  isPushing : false, // back button support
+
+  //set to true to print encountered errors
+  debug : true,
+  logMessage : function(msg) {
+    if (this.debug) {
+      if (false && window.console && window.console.log) window.console.log(msg)
+      else alert(msg)
+    }
+  },
+
+  //initialize Ajaxify library, hijack browser popstate
+  init : function() {
+    if (Ajaxify.isInitialized) return true //don't need to do this more than once
+    try {
+      if (window.history && window.history.pushState) {
+	
+	// Make the back button work for ajax links
+	// Used to detect initial (useless) popstate.
+	// If history.state exists, assume browser isn't going to fire initial popstate.
+	var isPopped = (window.history.state && window.history.state.href)
+	var initialURL = location.href
+
+	// popstate handler takes care of the back and forward buttons
+	$(window).bind('popstate', function(event){
+	  // Ignore inital popstate that some browsers fire on page load
+	  var initialPop = !isPopped && location.href == initialURL
+	  isPopped = true
+	  if (initialPop) return
+	  
+	  //try to handle our ajax state, or else use default browser behavior
+	  var state = history.state || event.state
+	  if (state) {
+	    Ajaxify.handle(state)
+	  }
+	  else {
+	    //this makes the back button work, albeit not via ajax, do what the browser would have done..
+	    window.location = location.href
+	  }
+	})
+
+	Ajaxify.isPushing = true
+      } //end if pushState supported
+
+    } catch(e) {
+      Ajaxify.logMessage("error during ajaxify initialization: " + e)
+    }
+    Ajaxify.isHandling = true //not used right now
+    Ajaxify.isInitialized = true
+  },
+
+  // Call this on a container of elements to have them handle clicks in an ajaxified way
+  // selector is optional, and a way to operate on descendants
+  // and options can be passed in its place
+  // Exported as $.fn.ajaxify
+  // Example Usage: 
+  //  $(document).ajaxify(".ajaxLink", {update: '#container'})
+  //  $("body .ajaxLink").ajaxify({update: '#container', handle : yourSpecialHandler})
+  //  $("body .ajaxLink").ajaxify()
+  ajaxify : function(selector, options) {
+    if (!Ajaxify.isInitialized) Ajaxify.init()
+    //Ajaxify.logMessage('called ajaxify('+selector+', '+options.toString())
+    if (!options && (typeof selector == "object")) options = selector
+    //add click event to elements
+
+    return this.on('click.ajaxify', selector, function(event) {
+      var opts = $.extend({}, (options||{}), Ajaxify.extractElementAttributes(this))
+
+      var doAjaxify = (opts.ajaxify != 'no') // ajaxify="no" means dont ajaxify this link
+      
+      // Middle click, cmd click, and ctrl click should use browser default behavior
+      if ( event.which > 1 || event.metaKey || event.ctrlKey || event.shiftKey || event.altKey )
+	return
+	
+      if (!doAjaxify)
+	return
+
+      if (Ajaxify.isPushing) {
+	opts.push = opts.push || 'push'
+	if (opts.replace) push = 'replace'
+	if (opts.push=='push' || opts.push=='yes') history.pushState(opts, '', this.href)
+	else if (opts.push=='replace') history.replaceState(opts, '', this.href)
+	// any other value of push means don't push it
+      }
+
+      Ajaxify.handle(opts) // handle this link
+
+      event.preventDefault() //jQuery way to prevent default behavior
+      return false // oldschool way to prevent default behavior
+    })
+  },
+
+  // extract element attributes to Object
+  // elm is a DOM Element or the ID of one
+  extractElementAttributes : function(elm) {
+    try {
+      if (typeof(elm) == "string") {
+	if (elm[0] == "#") elm = elm.substring(1,elm.length) //strip leading #
+	elm = document.getElementById(elm)
+      } // else assume it's a DOM Element
+      var s = {}
+      var attrs = elm.attributes
+      for (var i = 0; i < attrs.length; i++) {
+	s[attrs[i].nodeName] = attrs[i].nodeValue
+      }
+      return s
+    } catch(e) {
+      return null
+    }
+  },
+  
+  //handle an ajax state, which may define it's own handler
+  handle : function(state, handler) {
+    try {
+      handler = eval(handler || (state.handle || state.handler) || Ajaxify.defaultHandler)
+      handler(state)
+    }
+    catch(e) {
+      Ajaxify.logMessage('Ajaxify.handle() error!' + e)
+    }
+  },
+  
+  /* Handle state, do something ajaxy.. (Depends on jQuery)
+     This is the default thing to do:
+     1. Execute jsbefore javascript (if present)
+     2. Make a request to href?format=<format>
+     3. If update is passed, load that element with the result of the request
+     state (Object) representation of a link
+     href :  request URL
+     push : push into browser history?  Default is true. Pass push="no" to disable.
+     format : format to inject into request url
+     update : element(s) selector to update. Typically a single div id. Eg. #mycontent
+     jsbefore : javascript to execute before making request (always invoked)
+     onsuccess : request callback like function(data) {...}
+     onfailure : request callback like function(data) {...}
+     oncomplete : request callback like function(data) {...}
+     jsafter : javascript to execute after making request (only invoked if request is made)
+  */
+  // Depends on jQuery
+  defaultHandler : function(state) {
+    //parse options
+    var url = state.href || state.url
+    var format = state.format || Ajaxify.defaultRequestFormat
+    var divId = state.update || state.divid //update a single div, by ID
+    var spinner = (state.spinner != "no")
+
+    try { if (state.jsbefore && state.jsbefore!="") { eval(state.jsbefore) } } catch (e) {}
+
+    // making a request ?
+    if (url && url.indexOf("javascript:") == -1) {
+      // inject format= into url
+      if (format && url && url.indexOf("format=") == -1) {
+	if (url.indexOf("?") == -1) url += "?format="+format
+	else url += "&format="+format
+      }
+      // insert loading "spinner" inside the div, this can be hidden or moved about with css
+      // This should go unnoticed if it is not styled..
+      if (divId) { 
+	var elms = $(divId)
+	$(elms).prepend("<div class=\"ajax-spinner\"></div>")
+      }
+      var requestType = state['request-type'] || state['method'] 
+      // make request, execute callbacks, load div if successful and update element is present
+      $.ajax({
+	url: url,
+	type: (requestType || 'GET'),
+	data: null, //could serialize form(s) here
+	success: function(data) {
+	  if (divId) {
+	    var elms = $(divId)
+	    if (elms.length == 0) Ajaxify.logMessage("Container update element '"+divId+"' was not found!")
+	    //Replace the div content
+	    $(elms).html(data)
+	    //Ajaxify the loaded div
+	    //JD: The links are somehow already ajaxified.  How ?? Why ??
+	    // and uncommenting the following line causes major havok!
+	    //$(elms).ajaxify(state)
+	  }
+	  try { if (state.onsuccess && state.onsuccess!="") { eval(state.onsuccess)(data) } } catch (e) {}
+	},
+	failure: function(data) {
+	  try { if (state.onfailure && state.onfailure!="") { eval(state.onfailure)(data) } } catch (e) {}
+	},
+	complete: function(data) {
+	  try { if (state.oncomplete && state.oncomplete!="") { eval(state.oncomplete)(data) } } catch (e) {}
+	  try { if (state.jsafter && state.jsafter!="") { eval(state.jsafter) } } catch (e) {}
+	}
+      })
+    } // if url
+  }
+}; //end Ajaxify
+
+
+// Add ajaxify method to jQuery.fn
+(function($){
+  $.fn.ajaxify = Ajaxify.ajaxify
+})(jQuery);
+
+//Export Library (so you can compile this javascript)
+window['jQuery'] = jQuery;
+window['Ajaxify'] = Ajaxify;
+
